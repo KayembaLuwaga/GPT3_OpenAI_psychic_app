3d097f77b0a2cc35f201b8946efc62c34798c3b8
Kayemba Luwaga <76554604+KayembaLuwaga@users.noreply.github.com>
2023-12-28 22:05:21 +0300

    Error logging correction
    Request Errors can now be logged in the GUI

--- app.py
+++ app.py
@@ -5,23 +5,29 @@ app = Flask(__name__)
 
 # Dummy function for interacting with OpenAI API
 def get_openai_quotes(api_key, age_group, gender):
-    # Set the OpenAI API key dynamically based on user input
-    openai.api_key = api_key
+    try:
+        # Set the OpenAI API key dynamically based on user input
+        openai.api_key = api_key
 
-    # Define the prompt for the OpenAI API
-    prompt = f"Write excellent positive-well being vibes, motivation quotes for me, a person in age range {age_group}, my gender is {gender}"
+        # Define the prompt for the OpenAI API
+        prompt = f"Write excellent positive-well being vibes, motivation quotes for me, a person in age range {age_group}, my gender is {gender}"
 
-    # Make a request to the OpenAI API
-    response = openai.Completion.create(
-        engine="text-davinci-002",  # Use an appropriate engine
-        prompt=prompt,
-        max_tokens=100  # Adjust based on your needs
-    )
+        # Make a request to the OpenAI API
+        response = openai.Completion.create(
+            engine="text-davinci-002",  # Use an appropriate engine
+            prompt=prompt,
+            max_tokens=100  # Adjust based on your needs
+        )
 
-    # Extract the generated quote from the API response
-    generated_quote = response['choices'][0]['text']
+        # Extract the generated quote from the API response
+        generated_quote = response['choices'][0]['text']
 
-    return generated_quote
+        return generated_quote
+
+    except Exception as e:
+        # Log the error
+        app.logger.error(f"Error in get_openai_quotes: {e}")
+        return f"Error: {e}"
 
 @app.route('/')
 def index():
@@ -34,14 +40,17 @@ def get_quotes():
     age_group = request.form.get('age_group')
     gender = request.form.get('gender')
 
-    # Validate API key
-    # if api_key != 'your_api_key':  # Replace 'your_api_key' with your actual API key
-    #     return render_template('index.html', error='Invalid API key')
+    try:
+        # Include age group and gender in the request to OpenAI
+        openai_response = get_openai_quotes(api_key, age_group, gender)
 
-    # Include age group and gender in the request to OpenAI
-    openai_response = get_openai_quotes(api_key, age_group, gender)
+        return render_template('index.html', quotes=openai_response)
 
-    return render_template('index.html', quotes=openai_response)
+    except Exception as e:
+        # Log the error
+        app.logger.error(f"Error in get_quotes: {e}")
+
+        return render_template('index.html', error=f"Error: {e}")
 
 if __name__ == '__main__':
     app.run(debug=True)
